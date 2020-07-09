94b6acd40a32e375d3a4d59e7968657b42efa9f5
aeonincode <khashayar.sasannia@hotmail.com>
2020-07-09 15:17:28 +0200

    Testing with Deno 3

--- models/planets.test.ts
+++ models/planets.test.ts
@@ -6,4 +6,46 @@
  * 3. built-in test fixtures with Deno.test().
  */
 
-Deno.test();
+import {
+  assertEquals,
+  assertNotEquals,
+} from "https://deno.land/std/testing/asserts.ts";
+
+Deno.test("short example test", () => {
+  assertEquals("deno", "deno");
+  assertNotEquals({
+    runtime: "deno",
+  }, {
+    runtime: "node",
+  });
+});
+
+Deno.test({
+  name: "long example test",
+  ignore: Deno.build.os === "linux",
+  fn() {
+    assertEquals("deno", "deno");
+    assertNotEquals({
+      runtime: "deno",
+    }, {
+      runtime: "node",
+    });
+  },
+});
+
+Deno.test({
+  name: "operation leak",
+  sanitizeOps: false,
+  fn() {
+    setTimeout(console.log, 10000);
+  },
+});
+
+Deno.test({
+  name: "resource leak",
+
+  //sanitizeOps: false,
+  async fn() {
+    await Deno.open("./models/planets.ts");
+  },
+});
